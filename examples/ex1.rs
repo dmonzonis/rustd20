71a6639648bdd0870708d11fad6f4d903cb855a3
dmonzonis <dmonzonis@protonmail.com>
2020-08-07 21:34:47 +0200

    Allow negative modifiers

--- examples/ex1.rs
+++ examples/ex1.rs
@@ -5,5 +5,6 @@ fn main() {
     // Rolling dice
     println!("Rolling a 3d6: {}", dice::roll(6, 3));
     // Parsing dice rolls
-    println!("Rolling 1d20,1d8,2d12+5: {}", parser::parse_roll("1d20,1d8,2d12+5"));
+    let s = "1d20,1d8,2d12+5";
+    println!("Rolling {}: {}", s, parser::parse_roll(s));
 }
--- src/parser.rs
+++ src/parser.rs
@@ -3,23 +3,23 @@ use lazy_static::lazy_static;
 use regex::Regex;
 
 
-pub fn parse_roll(s: &str) -> u32 {
+pub fn parse_roll(s: &str) -> i32 {
     let mut total = 0;
     lazy_static! {
         static ref DIE_REGEX: Regex = Regex::new(
-            r"(\d+)d(\d+)(\+\d+)?"
+            r"(\d+)d(\d+)((\+|-)\d+)?"
         ).unwrap();
     }
     for caps in DIE_REGEX.captures_iter(s) {
         let times: u32 = caps.get(1).unwrap().as_str().parse().unwrap();
         let n: u32 = caps.get(2).unwrap().as_str().parse().unwrap();
-        let modifier: u32 = {
+        let modifier: i32 = {
             match caps.get(3) {
                 Some(num) => num.as_str().parse().unwrap(),
                 None => 0
             }
         };
-        total += roll(n, times) + modifier;
+        total += roll(n, times) as i32 + modifier;
     }
     total
 }
